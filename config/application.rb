f3b97c90d9eed30173fc7609461ad18491281b2f
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2016-09-26 21:26:30 +0300

    Styled the Layout and added the authentication system

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Arabia
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    I18n.enforce_available_locales = true
-    config.i18n.default_locale = :ar
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    # I18n.enforce_available_locales = true
+    # config.i18n.default_locale = :ar
   end
 end
